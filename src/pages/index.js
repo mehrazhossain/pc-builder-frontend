b111fc9c7fae3da7072737d15f8d5ea9c91ede9e
Mehraz <mehrazhossain54@gmail.com>
2023-08-12 00:47:40 +0600

    fix some syntax error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import { CgSmartphoneRam } from "react-icons/cg";
-import RootLayout from "../../components/Layouts/RootLayout";
 import { BsCpu, BsMotherboard } from "react-icons/bs";
 import { PiMonitor } from "react-icons/pi";
 import { ImPower } from "react-icons/im";
 import { MdStorage } from "react-icons/md";
 import { RxMixerHorizontal } from "react-icons/rx";
 import { Col, Divider, Row } from "antd";
+import ProductCard from "@/components/ProductCard";
+import RootLayout from "@/components/Layouts/RootLayout";
 import Link from "next/link";
-import ProductCard from "../../components/ProductCard";
 
 const featuredCategories = [
   {
@@ -71,141 +71,22 @@ const HomePage = ({ products }) => {
       </Divider>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         {featuredCategories.map((category) => (
-          <Col className="gutter-row" xs={24} md={6} span={6} key={category.id}>
-            <Link
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                padding: "10px",
-              }}
-            ></div>
-            <img
-              class="relative w-40"
-              src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
-              alt=""
-            />
-          </div>
-          <div class="relative text-white px-6 pb-6 mt-6">
-            <span class="block opacity-75 -mb-1">Ram</span>
-            <div class="">
-              <span class="font-semibold text-xl">
-                Peace Lily Peace Lily Peace Lil
-              </span>
-            </div>
-            <div class="flex justify-between pt-2">
-              <span class="text-sm items-center">⭐⭐⭐⭐⭐ (50)</span>
-              <span class="block bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
-                $45.00
-              </span>
-            </div>
-            <span class="block opacity-75 -mb-1 font-medium">In Stock</span>
-          </div>
-        </div>
-        <div class="flex-shrink-0 m-6 relative overflow-hidden bg-teal-500 rounded-lg max-w-xs shadow-lg">
-          <svg
-            class="absolute bottom-0 left-0 mb-8"
-            viewBox="0 0 375 283"
-            fill="none"
-            style={{ transform: "scale(1.5)", opacity: 0.1 }}
-          >
-            <rect
-              x="159.52"
-              y="175"
-              width="152"
-              height="152"
-              rx="8"
-              transform="rotate(-45 159.52 175)"
-              fill="white"
-            />
-            <rect
-              y="107.48"
-              width="152"
-              height="152"
-              rx="8"
-              transform="rotate(-45 0 107.48)"
-              fill="white"
-            />
-          </svg>
-          <div class="relative pt-10 px-10 flex items-center justify-center">
-            <div
-              class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-              style={{
-                background: "radial-gradient(black, transparent 60%)",
-                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
-                opacity: 0.2,
-              }}
-            ></div>
-            <img
-              class="relative w-40"
-              src="https://user-images.githubusercontent.com/2805249/64069998-305de300-cc9a-11e9-8ae7-5a0fe00299f2.png"
-              alt=""
-            />
-          </div>
-          <div class="relative text-white px-6 pb-6 mt-6">
-            <span class="block opacity-75 -mb-1">Ram</span>
-            <div class="">
-              <span class="font-semibold text-xl">
-                Peace Lily Peace Lily Peace Lil
-              </span>
-            </div>
-            <div class="flex justify-between pt-2">
-              <span class="text-sm items-center">⭐⭐⭐⭐⭐ (50)</span>
-              <span class="block bg-white rounded-full text-teal-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
-                $45.00
-              </span>
-            </div>
-            <span class="block opacity-75 -mb-1 font-medium">In Stock</span>
-          </div>
-        </div>
-        <div class="flex-shrink-0 m-6 relative overflow-hidden bg-purple-500 rounded-lg max-w-xs shadow-lg">
-          <svg
-            class="absolute bottom-0 left-0 mb-8"
-            viewBox="0 0 375 283"
-            fill="none"
-            style={{ transform: "scale(1.5)", opacity: 0.1 }}
-          >
-            <rect
-              x="159.52"
-              y="175"
-              width="152"
-              height="152"
-              rx="8"
-              transform="rotate(-45 159.52 175)"
-              fill="white"
-            />
-            <rect
-              y="107.48"
-              width="152"
-              height="152"
-              rx="8"
-              transform="rotate(-45 0 107.48)"
-              fill="white"
-            />
-          </svg>
-          <div class="relative pt-10 px-10 flex items-center justify-center">
-            <div
-              class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-              style={{
-                background: "radial-gradient(black, transparent 60%)",
-                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
-                opacity: 0.2,
-              }}
-            ></div>
-            <img
-              class="relative w-40"
-              src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
-              alt=""
-            />
-          </div>
-          <div class="relative text-white px-6 pb-6 mt-6">
-            <span class="block opacity-75 -mb-1">Ram</span>
-            <div class="">
-              <span class="font-semibold text-xl">
-                Peace Lily Peace Lily Peace Lil
-              </span>
-              <span className="font-medium">{category.name}</span>
-            </Link>
+          <Col className="gutter-row" xs={12} md={8} span={6}>
+            <li className="group list-none">
+              <Link href={category.url} className="flex gap-3 items-center">
+                <div className="w-12 h-12 rounded-full bg-indigo-50 text-indigo-600 flex items-center justify-center duration-150 group-hover:bg-indigo-600 group-hover:text-white md:w-14 md:h-14">
+                  {category.icon}
+                </div>
+                <div>
+                  <span className="text-gray-800 duration-200 group-hover:text-indigo-600 text-sm font-medium md:text-base">
+                    {category.name}
+                  </span>
+                  <p className="text-sm text-gray-600 group-hover:text-gray-800 mt-1">
+                    Duis aute irure dolor in reprehenderit
+                  </p>
+                </div>
+              </Link>
+            </li>
           </Col>
         ))}
       </Row>
